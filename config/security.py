def3ad8ddf442cb09fcc4cc3d60d510d543cdf54
joestarhu <joestarhu@163.com>
2024-07-31 20:57:12 +0800

    config file update

--- config/security.py
+++ config/security.py
@@ -1,57 +1,52 @@
 from jhu.security import AESAPI, HashAPI, JWTAPI
-from api.config.settings import settings
+from .settings import settings
 
-# 用于服务端的加密
-aes_api = AESAPI(settings.encrypt_key)
-# 用于客户端和服务端的加密
-client_aes_api = AESAPI(settings.hash_key)
-# hash加密
+
+jwt_api = JWTAPI(key=settings.jwt_key, expire_min=settings.jwt_expire_min)
+
 hash_api = HashAPI()
-# jwt加密
-jwt_api = JWTAPI(settings.jwt_key, settings.jwt_expire_min)
+
+# 用于处理客户端加密:比如加解密登录密码
+client_aes_api = AESAPI(settings.aes_key_32)
+
+# 用于处理服务内部加密:比如手机号的加解密
+server_aes_api = AESAPI(settings.aes_key_16)
 
 
-class FieldSecurity:
-    @staticmethod
-    def phone_encrypt(plain_text: str) -> str:
-        """加密手机号;手机号按照每3位组成一段密文,然后拼接而成;
-        如:18012345678,分为 180,801,012,123,......678;  
-        使用AES ECB模式加密,保障同样的明文输出同样的密文,用于手机号的模糊匹配
+def phone_encrypt(plain_text: str) -> str:
+    """加密手机号;手机号按照每3位组成一段密文,然后拼接而成;
+    如:18012345678,分为 180,801,012,123,......678;  
+    使用AES ECB模式加密,保障同样的明文输出同样的密文,用于手机号的模糊匹配
 
-        Args:
-            plain_text:手机号明文,例如:18012345678
+    Args:
+        plain_text:手机号明文,例如:18012345678
 
-        Return:
-            str:加密后的手机号
-        """
+    Return:
+        str:加密后的手机号
+    """
+    length = len(plain_text)
+    if length > 11:
+        raise ValueError("Phone Length need ≤ 11")
 
-        phone_len = len(plain_text)
-        if phone_len > 11:
-            raise ValueError("手机号长度必须小于11位")
-        end_pos = max(0, phone_len - 3)
+    end_pos = max(length-3, 0)
+    return ",".join([server_aes_api.encrypt(plain_text[i:i+3]) for i in range(end_pos+1)])
 
-        return ",".join([aes_api.encrypt(plain_text[i:i+3]) for i in range(end_pos+1)])
 
-    @staticmethod
-    def phone_decrypt(encrypted_text: str, mask: bool = True) -> str:
-        """解密手机号
+def phone_decrypy(encrypted_text: str, mask: bool = True) -> str:
+    """解密手机号
 
-        Args:
-            encrypted_text:加密的手机号
-            mask: 是否脱敏显示,比如180****5678
+    Args:
+        encrypted_text:加密的手机号
+        mask: 是否脱敏显示,比如180****5678
 
-        Return:
-            str:手机号明文
-        """
-        phone_array = [aes_api.decrypt(v) for v in encrypted_text.split(",")]
+    Return:
+        str:手机号明文
+    """
+    phone_array = [server_aes_api.decrypt(v)
+                   for v in encrypted_text.split(",")]
 
-        try:
-            phone = "".join([phone_array[i][0]
-                            for i in range(8)]) + phone_array[-1]
-        except Exception as e:
-            raise e
+    phone = "".join([phone_array[i][0] for i in range(8)]) + phone_array[-1]
 
-        if mask:
-            return f"{phone[:3]}****{phone[7:]}"
-        else:
-            return phone
+    if mask:
+        phone = f"{phone[:3]}****{phone[7:]}"
+    return phone
--- config/settings.py
+++ config/settings.py
@@ -2,43 +2,40 @@ from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
-class AppSettings(BaseSettings):
-    """App的配置信息
-    """
+class APISettings(BaseSettings):
+    """API配置文件"""
 
     model_config = ConfigDict(
         env_file=".env",
         extra="ignore"
     )
 
-    # FastAPI配置
+    # FastAPI配置信息
     docs_url: str = "/docs"
-    redoc_url: str = "/redoc"
-    title: str = "One"
+    redoc_url: str = ""
+    title: str = "one"
     version: str = "1.0.0"
+
     swagger_js_url: str = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.4/swagger-ui-bundle.js"
     swagger_css_url: str = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.4/swagger-ui.css"
 
     # CORS配置
     allow_origins: list[str] = ["*"]
     allow_credentials: bool = True
-    allow_methods: list[str] = ["POST", "GET"]
+    allow_methods: list[str] = ["*"]
     allow_headers: list[str] = ["*"]
 
     # 安全相关
     default_passwd: str = "default_passwd"
     encrypt_key: str = "encrypt_key"
-    hash_key: str = "hash_key"
-    jwt_key: str = "jwt_key"
+    aes_key_16: str = "0123456789ABCDEF"
+    aes_key_32: str = "0123456789ABCDEF0123456789ABCDEF"
+    jwt_key: str = "0123456789ABCDEF"
     jwt_expire_min: int = 60*24
 
     # 数据库,SQL
     db_rds: str = "mysql+pymysql://username:passwd@host:port/database"
     pool_recycle_seconds: int = 3600
-
-    # 钉钉的AK/SK设置
-    ding_ak: str = "ding_ak"
-    ding_sk: str = "ding_sk"
 
     @property
     def fastapi_kwargs(self) -> dict:
@@ -61,4 +58,4 @@ class AppSettings(BaseSettings):
         )
 
 
-settings = AppSettings()
+settings = APISettings()
