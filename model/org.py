8bee67bb62876f222e4779395ff487ea7dbfd90d
joestarhu <joestarhu@163.com>
2024-08-27 09:24:58 +0800

    user org update

--- model/org.py
+++ model/org.py
@@ -1,7 +1,6 @@
 from enum import Enum
-from sqlalchemy import Boolean, Integer, String, UniqueConstraint
-from sqlalchemy.orm import Mapped as M, mapped_column as mc
-from .base import ModelBase
+from sqlalchemy import Boolean, Integer, SmallInteger, String, UniqueConstraint
+from .base import ModelBase, M, mc
 
 
 class OrgStatus(Enum):
@@ -14,32 +13,92 @@ class OrgUserStatus(Enum):
     ENABLE = 1
 
 
+class OrgAppStatus(Enum):
+    DISABLE = 0
+    ENABLE = 1
+
+
 class Org(ModelBase):
     __tablename__ = "t_org"
     __table_args__ = (
         {"comment": "组织信息"}
     )
 
-    org_uuid: M[str] = mc(String(32), unique=True, comment="组织的uuid码")
-    org_name: M[str] = mc(String(128), unique=True, comment="组织名称")
-    owner_uuid: M[int] = mc(String(32), comment="组织所有者UUID")
-    remark: M[str] = mc(String(256), default="", comment="组织备注信息")
-    status: M[int] = mc(
-        Integer, default=OrgStatus.ENABLE.value, comment="组织状态")
-    is_admin: M[bool] = mc(Boolean, default=False, comment="是否为管理者组织")
-    is_deleted: M[bool] = mc(Boolean, default=False, comment="逻辑删除标识")
+    org_uuid: M[str] = mc(String(32),
+                          unique=True,
+                          default="",
+                          comment="组织UUID"
+                          )
+
+    org_name: M[str] = mc(String(64),
+                          unique=True,
+                          default="",
+                          comment="组织名称"
+                          )
+
+    owner_uuid: M[str] = mc(String(32),
+                            default="",
+                            comment="组织所有者UUID"
+                            )
+
+    remark: M[str] = mc(String(256),
+                        default="",
+                        comment="组织备注信息"
+                        )
+
+    status: M[int] = mc(SmallInteger,
+                        default=OrgStatus.ENABLE.value,
+                        comment="组织状态"
+                        )
+
+    is_deleted: M[bool] = mc(Boolean,
+                             default=False,
+                             comment="逻辑删除标识"
+                             )
 
 
 class OrgUser(ModelBase):
     __tablename__ = "t_org_user"
     __table_args__ = (
-        UniqueConstraint("org_uuid", "user_uuid", name="uni_org_user"),
+        UniqueConstraint("org_uuid", "user_uuid",
+                         name="uni_org_user"),
         {"comment": "组织用户信息"}
     )
 
-    org_uuid: M[int] = mc(String(32), comment="组织UUID")
-    user_uuid: M[int] = mc(String(32), comment="用户UUID")
-    user_name: M[str] = mc(String(128), comment="用户在组织内的名称")
-    avatar: M[str] = mc(String(512), default="", comment="用户在组织内的头像")
-    status: M[int] = mc(
-        Integer, default=OrgUserStatus.ENABLE.value, comment="用户在组织内的状态")
+    org_uuid: M[str] = mc(String(32),
+                          default="",
+                          comment="组织UUID"
+                          )
+
+    user_uuid: M[str] = mc(String(32),
+                           default="",
+                           comment="用户UUID"
+                           )
+
+    org_user_name: M[str] = mc(String(64),
+                               default="",
+                               comment="用户在组织内的名称"
+                               )
+
+    org_user_avatar_url: M[str] = mc(String(256),
+                                     default="",
+                                     comment="用户在组织内的头像URL"
+                                     )
+
+    org_user_status: M[int] = mc(SmallInteger,
+                                 default=OrgUserStatus.ENABLE.value,
+                                 comment="用户在组织内的状态"
+                                 )
+
+
+# class OrgApp(ModelBase):
+#     __tablename__ = "t_org_app"
+#     __table_args__ = (
+#         UniqueConstraint("org_uuid", "app_id", name="uni_org_user"),
+#         {"comment": "组织应用信息"}
+#     )
+
+#     org_uuid: M[int] = mc(String(32), comment="组织UUID")
+
+#     status: M[int] = mc(
+#         Integer, default=OrgAppStatus.ENABLE.value, comment="组织应用状态")
--- model/user.py
+++ model/user.py
@@ -1,7 +1,6 @@
 from enum import Enum
-from sqlalchemy import Boolean, Integer, String, UniqueConstraint
-from sqlalchemy.orm import Mapped as M, mapped_column as mc
-from .base import ModelBase
+from sqlalchemy import String, SmallInteger, Boolean, UniqueConstraint
+from .base import ModelBase, M, mc
 
 
 class UserStatus(Enum):
@@ -19,14 +18,44 @@ class User(ModelBase):
     __table_args__ = (
         {"comment": "用户信息"}
     )
-    user_uuid: M[str] = mc(String(32), unique=True, comment="用户UUID")
-    account: M[str] = mc(String(128), unique=True, comment="用户账户,唯一")
-    phone: M[str] = mc(String(256), unique=True, comment="用户手机号,加密存储")
-    nick_name: M[str] = mc(String(128), comment="用户昵称")
-    avatar: M[str] = mc(String(512), default="", comment="用户头像地址")
-    status: M[int] = mc(
-        Integer, default=UserStatus.ENABLE.value, comment="用户状态")
-    is_deleted: M[bool] = mc(Boolean, default=False, comment="逻辑删除标识")
+
+    user_uuid: M[str] = mc(String(32),
+                           unique=True,
+                           default="",
+                           comment="用户UUID"
+                           )
+
+    account: M[str] = mc(String(64),
+                         unique=True,
+                         default="",
+                         comment="用户账号"
+                         )
+
+    phone_enc: M[str] = mc(String(256),
+                           unique=True,
+                           default="",
+                           comment="用户手机号(加密)"
+                           )
+
+    nick_name: M[str] = mc(String(64),
+                           default="",
+                           comment="用户昵称"
+                           )
+
+    avatar_url: M[str] = mc(String(256),
+                            default="",
+                            comment="用户头像url"
+                            )
+
+    status: M[int] = mc(SmallInteger,
+                        default=UserStatus.ENABLE.value,
+                        comment="用户状态;0:停用,1:启用"
+                        )
+
+    is_deleted: M[bool] = mc(Boolean,
+                             default=False,
+                             comment="逻辑删除标识"
+                             )
 
 
 class UserAuth(ModelBase):
@@ -36,9 +65,23 @@ class UserAuth(ModelBase):
                          name="uni_user_auth"),
         {"comment": "用户认证信息"}
     )
-    user_uuid: M[str] = mc(String(32), comment="用户UUID")
-    auth_type: M[int] = mc(
-        Integer, default=UserAuthType.PASSWORD.value, comment="认证类型")
-    auth_identify: M[str] = mc(
-        String(128), default="", comment="认证标识,比如appid,appkey等")
-    auth_value: M[str] = mc(String(256), comment="认证值")
+
+    user_uuid: M[str] = mc(String(32),
+                           default="",
+                           comment="用户UUID"
+                           )
+
+    auth_type: M[int] = mc(SmallInteger,
+                           default=UserAuthType.PASSWORD.value,
+                           comment="认证类型"
+                           )
+
+    auth_identify: M[str] = mc(String(128),
+                               default="",
+                               comment="认证类型标识"
+                               )
+
+    auth_value: M[str] = mc(String(256),
+                            default="",
+                            comment="认证值"
+                            )
